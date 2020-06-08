1408fb4c487b41d9dcc6079ef78b1315bbdf9b69
Tara Thierry <tarathierry@gmail.com>
2020-06-07 19:30:28 -0500

    Add methods for interactivty

--- flask-crud-app/bookmanager.py
+++ flask-crud-app/bookmanager.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home() :
-    return "New flask app"
+    if request.form:
+        print(request.form)    
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
